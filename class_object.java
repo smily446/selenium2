1b8fa80098d2b4f6d7cad0b872d39dea0dd2fe55
smily446 <smily446@gmail.com>
2019-10-29 12:16:30 +0530

    java files

---
+++ class_object.java
@@ -0,0 +1,15 @@
+package practice_java;
+
+public class class_object {
+
+	public void validate()
+	{
+		System.out.println("hello i am learning jave code");
+	}
+	public static void main(String args[])
+	{
+	
+	}
+	
+	
+}
---
+++ class_object2.java
@@ -0,0 +1,11 @@
+package practice_java;
+
+public class class_object2 {
+
+	public static void main(String args[])
+	{
+		class_object cb=new class_object();
+		cb.validate();
+		
+	}
+}
